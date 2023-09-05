b0aec47f4736644e1aa189200fcb5cb65ff54264
Juan Nascimento <juannascimento048@gmail.com>
2023-09-05 08:10:43 -0300

    [Update] update for kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
 
 
